c243943ca5eea2ada6e7d9254a27d6b8f0b492e8
KishorSaravanan <kishor.saravana@vakilsearch.com>
2023-10-19 16:47:30 +0530

    first commit

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ApplicationRecord
 
 	paginates_per 5
-	validates :name, :phone_number, :date_of_birth, presence:true
+	validates :name, :phone_number, :date_of_birth, presence:true #comment
 end
